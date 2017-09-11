3b160265934ea0b9373830dab2bf8bb7dd4ce156
marving <gb694467942@126.com>
2017-09-11 09:38:24 +0800

    iqy

--- src/aliqiyi/aqy02.java
+++ src/aliqiyi/aqy02.java
@@ -1,6 +1,7 @@
 package aliqiyi;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Created by mercop on 2017/9/10.
@@ -11,40 +12,16 @@ public class aqy02 {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] nums = new int[n];
-
-        int max = 0;
         for (int i = 0; i < n; i++) {
             nums[i] = scanner.nextInt();
-            if(nums[i] > max) max = nums[i];
         }
-        visited = new boolean[max + 1];
-
+        long res = 1;
         Arrays.sort(nums);
-        getSelections(nums,0,new StringBuilder());
-        System.out.println(list.size());
+        for (int i = 0; i < nums.length; i++) {
+            res *= nums[i] - i;
+            if (res > 1000000007) res %= 1000000007;
+        }
+        System.out.println(res % 1000000007);
     }
 
-    static boolean visited[];
-
-    static Set<String> list = new HashSet<>();
-
-    public static void getSelections(int[] nums, int index, StringBuilder sb) {
-        if (nums.length == index){
-            list.add(sb.toString());
-            return;
-        }
-        for (int i = index; i < nums.length; i++) {
-            int x = nums[i];
-            while (x > 0) {
-                if (!visited[x]) {
-                    visited[x] = true;
-                    sb.append(x);
-                    getSelections(nums, index + 1,new StringBuilder(sb));
-                    visited[x] = false;
-                    sb.deleteCharAt(sb.length() -1);
-                }
-                x --;
-            }
-        }
-    }
 }
