7ad4e70d9bed96c69a663ac7fe0b8755f9fefad4
marving <gb694467942@126.com>
2017-09-17 11:55:20 +0800

    1

--- src/contest/stkj/stkj01.java
+++ src/contest/stkj/stkj01.java
@@ -1,5 +1,6 @@
 package contest.stkj;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 /**
@@ -11,16 +12,45 @@ public class stkj01 {
         int k = sc.nextInt();
         int n = sc.nextInt();
         int[][] nums = new int[k][n];
+        HashSet<Integer>[] memo = new HashSet[k];
         for(int i = 0; i < k;i ++){
+            HashSet hi = new HashSet();
             for(int j = 0; j < n; j++){
                 nums[i][j] = sc.nextInt();
+                hi.add(nums[i][j]);
             }
+            memo[i] = hi;
         }
         int min = Integer.MAX_VALUE;
         int max = -1;
         for(int i = 0; i < k ; i++){
             if(nums[i][0] > max) max = nums[i][0];
             if(nums[i][0] < min) min = nums[i][0];
+        }
+        int diff = max - min;
+
+        for(int j = 0; j < n;j ++){
+            int x = nums[0][j];
+            int minVal = Integer.MAX_VALUE;
+            int maxVal = -1;
+            for(int i = 0; i < k ; i++){
+                boolean found = false;
+
+                for(int z = x - diff > 0 ? x-diff : 0; z <= x + diff; z ++){
+                    HashSet<Integer> set = memo[j];
+                    if(set.contains(z)) {
+                        if(z > maxVal) maxVal = z;
+                        if(z < minVal) minVal = z;
+                        found = true;
+                    }
+                }
+                if(!found) break;
+                if(diff > maxVal - minVal) {
+                    diff = maxVal - minVal;
+                    min = minVal;
+                    max = maxVal;
+                }
+            }
         }
         System.out.println(min + " " + max);
 
